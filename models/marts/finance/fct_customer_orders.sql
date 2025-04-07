dbfefb59566abdabc98ffd078564334f3b8a5720
Rich Wyble <r-dbt-training@rpw-data.com>
2025-04-07 16:26:24 +0000

    WTF, do I have to commit before I build?  It seems that building stg_stripe__payments.sql is based on the github version not the ide version

--- models/marts/finance/fct_customer_orders.sql
+++ models/marts/finance/fct_customer_orders.sql
@@ -1,75 +1,105 @@
 with
-    -- Import CTEs
-    orders as (select * from {{ source("jaffle_shop", "orders") }}),
-    customers as (select * from {{ source("jaffle_shop", "customers") }}),
-    payments as (select * from {{ source("stripe", "payment") }}),
-    -- Logical CTEs
-    successful_payments as (
-        select
-            orderid as order_id,
-            max(created) as payment_finalized_date,
-            sum(amount) / 100.0 as total_amount_paid
-        from payments  -- raw.stripe.payment
-        where status <> 'fail'
-        group by 1
 
-    ),
+-- Import CTEs
 
-    paid_orders as (
-        select
-            orders.id as order_id,
-            orders.user_id as customer_id,
-            orders.order_date as order_placed_at,
-            orders.status as order_status,
-            successful_payments.total_amount_paid,
-            successful_payments.payment_finalized_date,
-            customers.first_name as customer_first_name,
-            customers.last_name as customer_last_name
-        from orders  -- raw.jaffle_shop.orders
-        left join successful_payments on orders.id = successful_payments.order_id
-        left join customers on orders.user_id = customers.id  -- raw.jaffle_shop.customers
-    ),
+customers as (
 
-    customer_orders as (
-        select
-            customers.id as customer_id,
-            min(order_date) as first_order_date,
-            max(order_date) as most_recent_order_date,
-            count(orders.id) as number_of_orders,
-        from customers  -- raw.jaffle_shop.customers
-        left join orders as orders on orders.user_id = customers.id  -- raw.jaffle_shop.orders
-        group by 1
-    ),
-    lifetime_value as (
-        select p.order_id, sum(t2.total_amount_paid) as customer_lifetime_value
-        from paid_orders p
-        left join
-            paid_orders t2
-            on p.customer_id = t2.customer_id
-            and p.order_id >= t2.order_id
-        group by 1
-    ),
-    -- Intermediate CTEs
-    -- Final CTEs
-    final as (
+  select * from {{ ref('stg_jaffle_shop__customers') }}
 
-        select
-            paid_orders.*,
-            row_number() over (order by paid_orders.order_id) as transaction_seq,
-            row_number() over (
-                partition by customer_id order by paid_orders.order_id
-            ) as customer_sales_seq,
-            case
-                when customer_orders.first_order_date = paid_orders.order_placed_at
-                then 'new'
-                else 'return'
-            end as nvsr,
-            lifetime_value.customer_lifetime_value,
-            customer_orders.first_order_date as fdos
-        from paid_orders
-        left join customer_orders using (customer_id)
-        left outer join lifetime_value on lifetime_value.order_id = paid_orders.order_id
-    )
-select *
-from final
---order by order_id
+),
+
+orders as (
+
+  select * from {{ ref('stg_jaffle_shop__orders') }}
+
+),
+
+payments as (
+
+  select * from {{ ref('stg_stripe__payments') }}
+
+),
+
+-- Logical CTEs
+
+completed_payments as (
+
+  select 
+    order_id,
+    max(payment_created_at) as payment_finalized_date,
+    sum(payment_amount) as total_amount_paid
+  from payments
+  where payment_status <> 'fail'
+  group by 1
+
+),
+
+paid_orders as (
+
+  select 
+    orders.order_id,
+    orders.customer_id,
+    orders.order_placed_at,
+    orders.order_status,
+
+    completed_payments.total_amount_paid,
+    completed_payments.payment_finalized_date,
+
+    customers.customer_first_name,
+    customers.customer_last_name
+  from orders
+  left join completed_payments on orders.order_id = completed_payments.order_id
+  left join customers on orders.customer_id = customers.customer_id
+
+),
+
+-- Final CTE
+
+final as (
+
+  select
+    order_id,
+    customer_id,
+    order_placed_at,
+    order_status,
+    total_amount_paid,
+    payment_finalized_date,
+    customer_first_name,
+    customer_last_name,
+
+    -- sales transaction sequence
+    row_number() over (order by order_id) as transaction_seq,
+
+    -- customer sales sequence
+    row_number() over (partition by customer_id order by order_id) as customer_sales_seq,
+
+    -- new vs returning customer
+    case  
+      when (
+      rank() over (
+      partition by customer_id
+      order by order_placed_at, order_id
+      ) = 1
+    ) then 'new'
+    else 'return' end as nvsr,
+
+    -- customer lifetime value
+    sum(total_amount_paid) over (
+      partition by customer_id
+      order by order_placed_at
+      ) as customer_lifetime_value,
+
+    -- first day of sale
+    first_value(order_placed_at) over (
+      partition by customer_id
+      order by order_placed_at
+      ) as fdos
+
+    from paid_orders
+		
+)
+
+-- Simple Select Statement
+
+select * from final
+order by order_id
